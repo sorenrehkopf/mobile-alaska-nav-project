7f89f305fe79aff028cc22124b5895e3fd00de06
Soren Smith <sorenrehkopfsmith@gmail.com>
2016-05-03 14:35:56 -0700

    basic styling set up. menu icon transition set up

--- public/js/controllers/NavCtrl.js
+++ public/js/controllers/NavCtrl.js
@@ -1,3 +1,18 @@
-app.controller('NavCtrl',function($scope,NavOptions){
+app.controller('NavCtrl',function($scope,NavOptions,$timeout){
 	$scope.navOptions = NavOptions.getNav();
+	$scope.section = 'Section';
+	$scope.location = 'Page Title';
+	$scope.menuToggle = function(){
+		if($scope.menuOpen){
+			$scope.menuOpen = false;
+			$scope.menuTrans = false;
+			$scope.closeIcon = false;
+		}else{
+			$scope.menuOpen = true;
+			$scope.menuTrans = true;
+			$timeout(function(){
+				$scope.closeIcon = true;
+			},100)
+		}
+	}
 })
